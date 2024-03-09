22c5132073468ea5bee0221f7f763aae90128257
Nikolas Papadopoulos <53314070+NikolasPpd@users.noreply.github.com>
2024-03-09 16:56:43 +0200

    Create navbar
    Use hardcoded user role until authentication is implemented

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,14 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script>
+    import "../app.pcss";
+    import Navbar from "$lib/components/Navbar.svelte";
+</script>
+
+<div class="flex flex-col min-h-screen">
+    <header>Αρχική</header>
+
+    <main class="flex flex-col p-4 w-full max-w-4xl box-border pb-16">
+        <slot />
+    </main>
+
+    <Navbar />
+</div>
