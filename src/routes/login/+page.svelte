31255ca9e2dc319cdfa9e5761f6d14a91989e3ac
Nikolas Papadopoulos <53314070+NikolasPpd@users.noreply.github.com>
2024-04-08 17:20:49 +0300

    Make UI a bit more desktop-friendly

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,38 +1,43 @@
 <script lang="ts">
-    import Button from "$lib/components/ui/button/button.svelte";
-    import Input from "$lib/components/ui/input/input.svelte";
-    import Label from "$lib/components/ui/label/label.svelte";
-	import { authHandlers, authStore } from "../../stores/authStore";
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
+	import Label from '$lib/components/ui/label/label.svelte';
+	import { authHandlers, authStore } from '../../stores/authStore';
 
-    let email = "";
-    let password = "";
+	let email = '';
+	let password = '';
 
-    let userRole = "admin"; // TODO: Replace with actual user role when authentication is implemented
+	let userRole = 'admin'; // TODO: Replace with actual user role when authentication is implemented
 
-    async function handleLogin() {
-        authHandlers.login(email, password).then(() => {
-            window.location.href = '/';
-        }).catch((error) => {
-            console.log(error.errorCode, error.errorMessage);
-        });
-    }
+	async function handleLogin() {
+		authHandlers
+			.login(email, password)
+			.then(() => {
+				window.location.href = '/';
+			})
+			.catch((error) => {
+				console.log(error.errorCode, error.errorMessage);
+			});
+	}
 </script>
 
-<div class="flex flex-col max-w-md w-full h-full justify-center items-stretch gap-8 p-2.5">
-    <div class="pt-4 text-zinc-700 text-5xl font-extrabold text-center">OrderFlow</div>
-    <form class="flex flex-col gap-8" on:submit|preventDefault={handleLogin}>
-        <div class="self-stretch rounded-lg flex-col justify-center items-start flex gap-4">
-            <div class="flex w-full flex-col gap-1.5 ">
-                <Label for="email">Email</Label>
-                <Input type="email" id="email" placeholder="" bind:value={email}/>
-            </div>
-            <div class="flex w-full flex-col gap-1.5 ">
-                <Label for="password">Κωδικός</Label>
-                <Input type="password" id="password" placeholder="" bind:value={password}/>
-            </div>
-        </div>
-        <div class="flex flex-col justify-center items-stretch">
-            <Button type="submit">Σύνδεση</Button>
-        </div>
-    </form>
+<div class="flex h-full w-full justify-center">
+	<div class="flex w-full max-w-md flex-col justify-center gap-8 p-2.5">
+		<div class="pt-4 text-center text-5xl font-extrabold text-zinc-700">OrderFlow</div>
+		<form class="flex flex-col gap-8" on:submit|preventDefault={handleLogin}>
+			<div class="flex flex-col items-start justify-center gap-4 self-stretch rounded-lg">
+				<div class="flex w-full flex-col gap-1.5">
+					<Label for="email">Email</Label>
+					<Input type="email" id="email" placeholder="" bind:value={email} />
+				</div>
+				<div class="flex w-full flex-col gap-1.5">
+					<Label for="password">Κωδικός</Label>
+					<Input type="password" id="password" placeholder="" bind:value={password} />
+				</div>
+			</div>
+			<div class="flex flex-col items-stretch justify-center">
+				<Button type="submit">Σύνδεση</Button>
+			</div>
+		</form>
+	</div>
 </div>
