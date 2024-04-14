a37666dc9dd18defbf9accfc06af92f92845350c
Nikolas Papadopoulos <53314070+NikolasPpd@users.noreply.github.com>
2024-04-14 03:07:16 +0300

    Implement API endpoints to create, update and delete users

--- src/routes/api/customers/+server.ts
+++ src/routes/api/customers/+server.ts
@@ -2,6 +2,7 @@
 
 import type { RequestHandler } from '@sveltejs/kit';
 import sql from '$lib/db';
+import { createUser } from '$lib/firebaseAdmin';
 
 /*
  * GET: Fetches all customers from the database.
@@ -35,14 +36,42 @@ export const GET: RequestHandler = async () => {
 export const POST: RequestHandler = async ({ request }) => {
 	const data = await request.json();
 
-	// TODO: Validate the request body to ensure all required fields are present and call the firebase function to create a new user
+	// TODO: Validate the request body to ensure all required fields are present
 
 	try {
+		// Check if the unique fields already exist in the database
+		const existingUser = await sql`
+			SELECT * FROM users
+			WHERE email = ${data.email} OR user_code = ${data.user_code} OR afm = ${data.afm} OR company_name = ${data.company_name}
+		`;
+
+		if (existingUser.length > 0) {
+			return new Response(
+				JSON.stringify({ error: 'User with provided unique fields already exists' }),
+				{
+					status: 400,
+					headers: { 'Content-Type': 'application/json' }
+				}
+			);
+		}
+
+		// Create a new user using firebaseAdmin. If the user is created successfully
+		// userRecord will contain the user's UID
+		// Docs: https://firebase.google.com/docs/auth/admin/manage-users#create_a_user
+		const userRecord = await createUser(data.email, 'password');
+		const uid = userRecord?.uid;
+		if (!uid) {
+			return new Response(JSON.stringify({ error: 'Failed to create user' }), {
+				status: 500,
+				headers: { 'Content-Type': 'application/json' }
+			});
+		}
+
 		const result = await sql`
 			INSERT INTO users
 			(firebase_uid, company_name, user_code, email, afm, phone_number, street_address, city, postal_code, role)
 			VALUES
-			(${data.firebase_uid}, ${data.company_name}, ${data.user_code}, ${data.email}, ${data.afm}, ${data.phone_number || null},
+			(${uid}, ${data.company_name}, ${data.user_code}, ${data.email}, ${data.afm}, ${data.phone_number || null},
 			${data.street_address || null}, ${data.city || null}, ${data.postal_code || null}, 'customer')
 			RETURNING *;
 		`;
@@ -52,7 +81,7 @@ export const POST: RequestHandler = async ({ request }) => {
 			headers: { 'Content-Type': 'application/json' }
 		});
 	} catch (error) {
-		console.error('Failed to create product:', error);
+		console.error('Failed to create user:', error);
 		return new Response(JSON.stringify({ error: 'Internal server error' }), {
 			status: 500,
 			headers: { 'Content-Type': 'application/json' }
--- src/routes/api/customers/[id]/+server.ts
+++ src/routes/api/customers/[id]/+server.ts
@@ -2,6 +2,7 @@
 
 import sql from '$lib/db';
 import type { RequestHandler } from '@sveltejs/kit';
+import { updateEmail, deleteUser } from '$lib/firebaseAdmin';
 
 /*
  * GET: Fetches a single customer by ID from the database.
@@ -43,6 +44,145 @@ export const GET: RequestHandler = async ({ params }) => {
 		}
 	} catch (error) {
 		return new Response(JSON.stringify({ error: 'Failed to fetch customer' }), {
+			status: 500,
+			headers: { 'Content-Type': 'application/json' }
+		});
+	}
+};
+
+/*
+ * PATCH: Updates a customer's details in the database.
+ * Validates the provided ID and request body to ensure they're valid.
+ * Returns the updated customer details on success or an error message on failure.
+ */
+export const PATCH: RequestHandler = async ({ params, request }) => {
+	const id = params.id;
+	if (!id) {
+		return new Response(JSON.stringify({ error: 'Customer ID is required' }), {
+			status: 400
+		});
+	}
+
+	// Safely convert id to a number
+	const customerId = parseInt(id, 10);
+	if (isNaN(customerId)) {
+		return new Response(JSON.stringify({ error: 'Invalid customer ID' }), {
+			status: 400
+		});
+	}
+
+	const data = await request.json();
+
+	try {
+		const user = await sql`
+            SELECT * FROM users
+            WHERE user_id = ${id};
+        `;
+
+		if (user.length === 0) {
+			return new Response(JSON.stringify({ error: 'Customer not found' }), {
+				status: 404,
+				headers: { 'Content-Type': 'application/json' }
+			});
+		}
+
+		// If the email is being updated, update it in Firebase too
+		if (data.email && data.email !== user[0].email) {
+			const emailUpdated = await updateEmail(user[0].firebase_uid, data.email);
+			if (!emailUpdated) {
+				return new Response(JSON.stringify({ error: 'Failed to update email in Firebase' }), {
+					status: 500,
+					headers: { 'Content-Type': 'application/json' }
+				});
+			}
+		}
+
+		const result = await sql`
+            UPDATE users
+            SET company_name = ${data.company_name}, user_code = ${data.user_code}, email = ${data.email}, phone_number = ${data.phone_number}, afm = ${data.afm}, street_address = ${data.street_address}, city = ${data.city}, postal_code = ${data.postal_code}
+            WHERE user_id = ${id}
+            RETURNING *;
+        `;
+
+		if (result.length > 0) {
+			return new Response(JSON.stringify(result[0]), {
+				headers: { 'Content-Type': 'application/json' },
+				status: 200
+			});
+		} else {
+			return new Response(JSON.stringify({ error: 'Customer not found' }), {
+				status: 404,
+				headers: { 'Content-Type': 'application/json' }
+			});
+		}
+	} catch (error) {
+		return new Response(JSON.stringify({ error: 'Failed to update customer' }), {
+			status: 500,
+			headers: { 'Content-Type': 'application/json' }
+		});
+	}
+};
+
+/*
+ * DELETE: Deletes a customer from the database.
+ * Validates the provided ID to ensure it's numeric and exists within the database.
+ * Returns a success message on successful deletion or an error message on failure.
+ */
+export const DELETE: RequestHandler = async ({ params }) => {
+	const id = params.id;
+	if (!id) {
+		return new Response(JSON.stringify({ error: 'Customer ID is required' }), {
+			status: 400
+		});
+	}
+
+	// Safely convert id to a number
+	const customerId = parseInt(id, 10);
+	if (isNaN(customerId)) {
+		return new Response(JSON.stringify({ error: 'Invalid customer ID' }), {
+			status: 400
+		});
+	}
+
+	try {
+		const user = await sql`
+            SELECT * FROM users
+            WHERE user_id = ${id};
+        `;
+
+		if (user.length === 0) {
+			return new Response(JSON.stringify({ error: 'Customer not found' }), {
+				status: 404,
+				headers: { 'Content-Type': 'application/json' }
+			});
+		}
+
+		// Delete the user from Firebase
+		if (await deleteUser(user[0].firebase_uid)) {
+			const result = await sql`
+				DELETE FROM users
+				WHERE user_id = ${id};
+			`;
+
+			if (result.count > 0) {
+				return new Response(JSON.stringify({ message: 'Customer deleted successfully' }), {
+					headers: { 'Content-Type': 'application/json' },
+					status: 200
+				});
+			} else {
+				return new Response(JSON.stringify({ error: 'Customer not found' }), {
+					status: 404,
+					headers: { 'Content-Type': 'application/json' }
+				});
+			}
+		} else {
+			return new Response(JSON.stringify({ error: 'Failed to delete user from Firebase' }), {
+				status: 500,
+				headers: { 'Content-Type': 'application/json' }
+			});
+		}
+	} catch (error) {
+		return new Response(JSON.stringify({ error: 'Failed to delete customer' }), {
 			status: 500,
 			headers: { 'Content-Type': 'application/json' }
 		});
