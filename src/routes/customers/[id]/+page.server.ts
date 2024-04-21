c1d3571114e8ef36e9b6db61a8003e47ce11a548
Nikolas Papadopoulos <53314070+NikolasPpd@users.noreply.github.com>
2024-04-21 23:09:14 +0300

    Add form validation to the Customer edit page

--- src/routes/customers/[id]/+page.server.ts
+++ src/routes/customers/[id]/+page.server.ts
@@ -1,6 +1,10 @@
 import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, Actions } from './$types';
 import type { Customer } from '$lib/types';
+import { customerSchema } from '$lib/schemas/customerSchema';
+import { zod } from 'sveltekit-superforms/adapters';
+import { message, superValidate, fail } from 'sveltekit-superforms';
+import humps from 'humps';
 
 export const load: PageServerLoad = async ({ locals, fetch, params }) => {
 	if (!locals.user) {
@@ -47,12 +51,68 @@ export const load: PageServerLoad = async ({ locals, fetch, params }) => {
 		}
 
 		return {
-			customer
+			customer,
+			form: await superValidate(
+				{
+					customerId: customer.user_id,
+					companyName: customer.company_name,
+					userCode: customer.user_code,
+					email: customer.email,
+					phoneNumber: customer.phone_number,
+					afm: customer.afm,
+					streetAddress: customer.street_address,
+					city: customer.city,
+					postalCode: customer.postal_code,
+					isAccountDisabled: customer.is_account_disabled
+				},
+				zod(customerSchema)
+			)
 		};
 	} catch (error) {
 		return {
 			status: 404,
 			error: new Error('Customer not found')
 		};
+	}
+};
+
+export const actions: Actions = {
+	editCustomer: async (event) => {
+		const form = await superValidate(event, zod(customerSchema));
+		if (!form.valid) {
+			return fail(400, {
+				form
+			});
+		}
+
+		// Convert form data to snake_case with humps library
+		// Assert the type to match the Customer interface
+		const { customerId, ...rest } = form.data;
+		const formData = humps.decamelizeKeys({ userId: customerId, ...rest }) as Customer;
+
+		try {
+			// TODO: Maybe only send the fields that have changed
+			const response = await event.fetch(`/api/customers/${formData.user_id}`, {
+				method: 'PATCH',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(formData)
+			});
+
+			if (!response.ok) {
+				const errorMessage = await response.text();
+				throw new Error(`Failed to update customer due to bad response: ${errorMessage}`);
+			}
+
+			// Returning the form is required for the superform validation to work
+			return message(form, { status: 'success', text: 'Οι αλλαγές αποθηκεύτηκαν επιτυχώς' });
+		} catch (error) {
+			console.error(`Failed to update customer: ${error}`);
+			return message(form, {
+				status: 'failed',
+				text: 'Υπήρξε πρόβλημα κατά την αποθήκευση των αλλαγών'
+			});
+		}
 	}
 };
