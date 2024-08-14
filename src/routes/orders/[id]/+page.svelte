c442caaecf55e5980db1ffebc09cedf013cbedd2
Panos Mavromatis <Panosmavromatis91@gmail.com>
2024-08-15 02:05:17 +0300

    Added select box to update order status.

--- src/routes/orders/[id]/+page.svelte
+++ src/routes/orders/[id]/+page.svelte
@@ -11,6 +11,8 @@
 	import pdfFonts from 'pdfmake/build/vfs_fonts';
 	import type { TDocumentDefinitions, Content, StyleDictionary } from 'pdfmake/interfaces';
 	import ordersStore from '../../../stores/ordersStore';
+	import * as Select from '$lib/components/ui/select';
+	import type { Selected } from 'bits-ui';
 
 	// Get order data from the server to populate the fields
 	export let data;
@@ -18,6 +20,16 @@
 
 	if (order === undefined) {
 		throw new Error('Order not found');
+	}
+	
+	const statuses = [
+		{ value: "pending", label: "Σε εκκρεμότητα" },
+		{ value: "complete", label: "Ολοκληρωμένη" }
+	];
+
+	let currentStatus = {
+		value: order.status,
+		label: statuses.find(status => status.value == order.status)?.label
 	}
 
 	let goHome: boolean;
@@ -142,6 +154,43 @@
 
 		pdfMake.createPdf(docDefinition).open();
 	};
+
+	// Update the order status.
+	async function handleStatusChange(s: Selected<string>) {
+    if (s) {
+
+		// Updates current status variable.
+        currentStatus = {
+            value: s.value as "pending" | "complete",
+            label: s.label
+        };
+
+		// Request to change status in the database
+        try {
+            const response = await fetch(`/api/orders/${order.order_id}`, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ status: currentStatus.value })
+            });
+
+            if (response.ok) {
+
+				// Finds order in the Store and updates its status.
+				let orderToUpdate = $ordersStore.find(o => o.order_id == order.order_id);
+				orderToUpdate.status = currentStatus.value;
+
+                toast.success('Κατάσταση παραγγελίας ενημερώθηκε επιτυχώς');
+            } else {
+                const error = await response.json();
+                toast.error(`Σφάλμα: ${error.error || 'Δεν ήταν δυνατή η ενημέρωση της κατάστασης'}`);
+            }
+        } catch (error) {
+            toast.error('Σφάλμα κατά την επικοινωνία με τον διακομιστή');
+        }
+    }
+}
 </script>
 
 <div class="flex flex-col gap-2.5 p-2.5">
@@ -199,7 +248,7 @@
 				</Card.Content>
 			</Card.Root>
 		{/if}
-		<Card.Root class="overflow-hidden">
+		<Card.Root class="overflow-hidden" >
 			<Card.Header class="p-0">
 				<Card.Title class="flex h-10 items-center justify-center border-b bg-secondary font-normal">
 					Καλάθι
@@ -211,6 +260,20 @@
 				{/each}
 			</Card.Content>
 		</Card.Root>
+		{#if userRole === 'admin'}
+		<p>Κατάσταση παραγγελίας *</p>
+			<Select.Root bind:selected={currentStatus} onSelectedChange={(s) => handleStatusChange(s)}>
+				<Select.Input />
+				<Select.Trigger class="w-full p-1 mb-2">
+					<Select.Value />
+				</Select.Trigger>
+				<Select.Content sameWidth={true} align="end" alignOffset={4} class="w-[110px] sm:w-[120px]">
+					{#each statuses as status}
+						<Select.Item value={status.value} label={status.label} />
+					{/each}
+				</Select.Content>
+			</Select.Root>
+		{/if}
 		<Button on:click={handlePrint}>Εκτύπωση</Button>
 	{/if}
 </div>
