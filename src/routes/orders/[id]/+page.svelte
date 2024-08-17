a16ca8cc8ea5710d4efe3866d99554cf191ccc4d
Constantinos Vakratsas <c.vakratsas@gmail.com>
2024-08-17 21:25:20 +0300

    npm run build command is now running

--- src/routes/orders/[id]/+page.svelte
+++ src/routes/orders/[id]/+page.svelte
@@ -8,7 +8,7 @@
 	import { goto } from '$app/navigation';
 	import { returnToHome } from '../../../stores/orderNavigationStore';
 	import pdfMake from 'pdfmake/build/pdfmake';
-	import pdfFonts from 'pdfmake/build/vfs_fonts';
+	import * as pdfFonts from 'pdfmake/build/vfs_fonts';
 	import type { TDocumentDefinitions, Content, StyleDictionary } from 'pdfmake/interfaces';
 	import ordersStore from '../../../stores/ordersStore';
 	import * as Select from '$lib/components/ui/select';
