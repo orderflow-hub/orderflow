f919dd5e2c7019a9b7314dd726d84310460b3fbe
Constantinos Vakratsas <c.vakratsas@gmail.com>
2024-09-22 13:04:13 +0300

    Fixed responsiveness issue with safari

--- src/lib/shared/CartEntry.svelte
+++ src/lib/shared/CartEntry.svelte
@@ -13,15 +13,15 @@
 		<Card.Content class="aspect-square h-[72px] p-0">
 			{#if product.image_url}
 				<img
-					class={cn('aspect-square h-full object-cover', {
+					class={cn('w-full object-cover', {
 						grayscale: product.is_disabled
 					})}
 					src={product.image_url}
 					alt="Εικόνα προϊόντος"
 				/>
 			{:else}
-				<div class="flex h-full w-full items-center justify-center">
-					<Image strokeWidth={0.6} size={158.2} />
+				<div class="flex w-full items-center justify-center object-cover">
+					<Image strokeWidth={0.6} class="h-full w-full object-contain" />
 				</div>
 			{/if}
 		</Card.Content>
