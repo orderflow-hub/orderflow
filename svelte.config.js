1a2b2b6b8496733c5a5f577a761e513a741517f8
Mavromatis Panagiotis <Panosmavromatis91@gmail.com>
2025-01-01 02:34:37 +0200

    Added `$stores` alias for easy importing of stores

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,6 +9,9 @@ const config = {
 	preprocess: [vitePreprocess({})],
 
 	kit: {
+		alias: {
+			$stores: path.resolve('./src/stores')
+		},
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
